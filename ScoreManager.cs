65d755ddcee54334e333753c127d5c97836aac19
BSAvatar <123509774+BSAvatar@users.noreply.github.com>
2025-04-06 20:52:35 -0400

    Create ScoreManager.cs

---
+++ ScoreManager.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
